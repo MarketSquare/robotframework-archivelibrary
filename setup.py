2c2617c5f938f38ce69a48bd04026ef83690f63d
Bulkan Evcimen <bulkan@gmail.com>
2012-09-28 17:34:27 +1000

    working on setup.py file

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+from os.path import abspath, dirname, join
+execfile(join(dirname(abspath(__file__)), 'RequestsArchive', 'version.py'))
+
+DESCRIPTION = """
+Robot Framework keyword library for handling ZIP files.
+"""[1:-1]
+
+
+CLASSIFIERS = """
+Development Status :: 5 - Production/Stable
+License :: Public Domain
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Software Development :: Testing
+"""[1:-1]
+
+setup(name         = 'robotframework-archivelibrary',
+      version      = VERSION,
+      description  = 'Robot Framework keyword library for handling ZIP files'
+      long_description = DESCRIPTION,
+      author       = 'Bulkan Savun Evcimen',
+      author_email = 'bulkan@gmail.com',
+      url          = 'http://github.com/bulkan/robotframework-archivelibrary',
+      license      = 'Public Domain',
+      keywords     = 'robotframework testing test automation zip files compresssed',
+      platforms    = 'any',
+      classifiers  = CLASSIFIERS.splitlines(),
+      package_dir  = {'' : 'ArchiveLibrary'},
+      packages     = ['ArchiveLibrary'],
+      package_data = {'ArchiveLibrary': ['tests/*.txt']}
+      )
