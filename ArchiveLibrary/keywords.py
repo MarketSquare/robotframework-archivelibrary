ce66d908f919d59da396a54217c56b754762d477
Bulkan Evcimen <bulkan@gmail.com>
2012-09-28 17:27:58 +1000

    extract keyword working

--- ArchiveLibrary/keywords.py
+++ ArchiveLibrary/keywords.py
@@ -1,17 +1,44 @@
 import os
 import zipfile
 
+from robot.libraries.BuiltIn import BuiltIn
+
+from robot.libraries.Collections import Collections
+
+from utils import Unzip
+
 
 class ArchiveKeywords(object):
     ROBOT_LIBRARY_SCOPE = 'Global'
 
-    def extract(self, p):
-        z = zipfile.ZipFile(p)
-        for f in z.namelist():
-            if f.endswith('/'):
-                os.makedirs(f)
+    def __init__(self):
+        self.oslib = BuiltIn().get_library_instance("OperatingSystem")
+        self.collections = Collections()
+
+    def extract_zip_file(self, zfile, dest=None):
+        if dest:
+            self.oslib.create_directory(dest)
+            self.oslib.directory_should_exist(dest)
+
+        cwd = os.getcwd()
+        os.chdir(dest)
+
+        unzipper = Unzip()
+
+        try:
+            unzipper.extract(zfile, dest)
+        except:
+            raise
+        finally:
+            os.chdir(cwd)
+
+    def archive_should_contain_file(self, zfile, filename):
+        self.oslib.file_should_exist(zfile)
+
+        z = zipfile.ZipFile(zfile)
+        self.collections.list_should_contain_value(z.namelist(), filename)
 
 
 if __name__ == '__main__':
     al = ArchiveKeywords()
-    al.extract('master.zip')
+    al.extract('test.zip')
---
+++ ArchiveLibrary/utils.py
@@ -0,0 +1,53 @@
+import os
+import zipfile
+
+""" Originally from
+
+ http://code.activestate.com/recipes/252508/
+
+"""
+
+
+class Unzip(object):
+
+    def extract(self, file, dir):
+        if not dir.endswith(':') and not os.path.exists(dir):
+            os.mkdir(dir)
+
+        zf = zipfile.ZipFile(file)
+
+        # create directory structure to house files
+        self._createstructure(file, dir)
+
+        # extract files to directory structure
+        for i, name in enumerate(zf.namelist()):
+            if not name.endswith('/'):
+                outfile = open(os.path.join(dir, name), 'wb')
+                outfile.write(zf.read(name))
+                outfile.flush()
+                outfile.close()
+
+    def _createstructure(self, file, dir):
+        self._makedirs(self._listdirs(file), dir)
+
+    def _makedirs(self, directories, basedir):
+        """ Create any directories that don't currently exist """
+        for dir in directories:
+            curdir = os.path.join(basedir, dir)
+            if not os.path.exists(curdir):
+                os.mkdir(curdir)
+
+    def _listdirs(self, file):
+        """ Grabs all the directories in the zip structure
+        This is necessary to create the structure before trying
+        to extract the file to it. """
+
+        zf = zipfile.ZipFile(file)
+
+        dirs = []
+
+        for name in zf.namelist():
+            if name.endswith('/'):
+                dirs.append(name)
+
+        return dirs
