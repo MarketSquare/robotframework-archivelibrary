0c7efd0f2b92aa908d6901391c2fcaef9d449574
Todor Minakov <todor.minakov@xogito.com>
2017-03-27 12:19:07 +0300

    + method which returns all files in a directory, optionally - in its subdirectories

--- ArchiveLibrary/utils.py
+++ ArchiveLibrary/utils.py
@@ -12,7 +12,6 @@ import tarfile
 
 
 class Archive(object):
-
     def _createstructure(self, file, dir):
         self._makedirs(self._listdirs(file), dir)
 
@@ -76,6 +75,30 @@ class Untar(Archive):
 
         tff = tarfile.open(name=tfile)
         return [name for name in tff.getnames() if name.endswith('/')]
+
+
+def return_files_lists(directory, include_sub_directories=False):
+    """ Returns the files in a given directory, and optionally it's subdirectories.
+        The return value is a list of tuples, the 1st tuple member - the file's path, 
+          the 2nd - its name for the archive. """
+
+    result = []
+
+    for path, _, files in os.walk(directory):
+        for target_file in files:
+            file_to_archive = os.path.join(path, target_file)
+            # generate the "relative" path by getting rid of the starting directory
+            file_name = path.replace(directory, '')
+            # the final filename is the relative path plus the file's name
+            file_name = os.path.join(file_name, target_file)
+
+            result.append((file_to_archive, file_name))
+
+        # if files in subdirs should not be returned, stop at the top-level folder
+        if not include_sub_directories:
+            break
+
+    return result
 
 
 if __name__ == "__main__":
